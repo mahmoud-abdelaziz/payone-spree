8f853ec3fed91fe63c7db5bec25e7f2a55adbc05
Maciej Gowin <maciej.abacus.gowin@gmail.com>
2013-02-23 22:50:55 +0000

    Changing default value for payone_request_history_entry.payment_id

--- db/migrate/20120803143111_create_payone_request_history_entries.spree_payone.rb
+++ db/migrate/20120803143111_create_payone_request_history_entries.spree_payone.rb
@@ -3,7 +3,7 @@ class CreatePayoneRequestHistoryEntries < ActiveRecord::Migration
     create_table :spree_payone_request_history_entries do |t|
       t.string :txid, :request_type, :status, :success_token, :back_token, :error_token
       t.boolean :overall_status
-      t.integer :payment_id, :default => :null, :null => true
+      t.integer :payment_id, :default => nil, :null => true
       
       t.timestamps
     end
