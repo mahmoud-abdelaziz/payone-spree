6ac528ec60e89900a9c08ddcae89230bed8f8495
Fabian Aussems <fabianaussems@gmail.com>
2013-05-10 16:37:40 +0200

    incompatible with heroku

--- app/models/spree/payone_request_history_entry.rb
+++ app/models/spree/payone_request_history_entry.rb
@@ -3,7 +3,7 @@ module Spree
   class PayoneRequestHistoryEntry < ActiveRecord::Base
     # we must run logger within new connection to avoid changes to be rollbacked
     # for details see: state_machine documentation
-    establish_connection Rails.env.to_sym
+#    establish_connection Rails.env.to_sym
     
     attr_accessible :txid, :request_type, :status, :overall_status
   end
